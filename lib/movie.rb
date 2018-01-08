6fff1aaae7aea9dc21995176e5f5900bceb6ed13
TravisA <github email address>
2018-01-07 20:24:14 -0700

    initialize checks for :chain and :show_times to skip them, then uses add_additional_attributes to convert them. create_from_scraper iterates without using @@all.each.

--- lib/movie.rb
+++ lib/movie.rb
@@ -1,23 +1,36 @@
+require "pry"
+
 class Movie
-	attr_accessor :title, :rating, :length, :carmike, :regal
+	attr_accessor :title, :rating, :length, :cinemark, :regal
 	@@all = []
 	def initialize(movie_hash)
 		movie_hash.each do |key, value| 
-			self.send(("#{key}="), value)
+			if key != :chain && key != :show_times
+				self.send(("#{key}="), value)
+			end
 		end
+		add_additional_attributes(self, movie_hash)
 		@@all << self
 	end
 
 	def self.create_from_scraper(movie_array)
 		movie_array.each do |movie_hash|
-			@@all.each do |movie_in_all|
-				if movie_in_all.title == movie_hash[:title]
-					add_additional_attributes(movie_in_all, movie_hash)
-				else
-					Movie.new(movie)
-				end
+			
+			current_movies_in_all = self.all.collect do |movie|
+				movie.title
 			end
+			if current_movies_in_all.include?(movie_hash[:title])
+				movie_in_all = self.all.search do |movie|
+					movie.title == movie_hash[:title]
+				end
+				add_additional_attributes(movie_in_all, movie_hash)
+			else
+				Movie.new(movie_hash)
+			end
+			
 		end
+		#binding.pry
+		
 	end
 
 	def self.all
@@ -25,8 +38,8 @@ class Movie
 	end
 
 	def add_additional_attributes(movie, movie_hash)
-		if movie_hash[:chain] == "carmike"
-			movie.carmike = movie_hash[:show_times]
+		if movie_hash[:chain] == "cinemark"
+			movie.cinemark = movie_hash[:show_times]
 		elsif movie_hash[:chain] == "regal"
 			movie.regal = movie_hash[:show_times]
 		end
@@ -34,8 +47,8 @@ class Movie
 
 	def self.show_times
 		self.all.each_with_index do |movie, i|
-			puts "#{i}. #{movie.title}"
-			puts "Carmike: #{movie.carmike}"
+			puts "#{i+1}. #{movie.title}"
+			puts "Carmike: #{movie.cinemark}"
 			puts "Regal: #{movie.regal}"
 		end
 	end
