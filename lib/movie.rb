ddeb47c0887d1e6107933a0fed727587396b6c71
TravisA <github email address>
2018-01-07 20:51:15 -0700

    changed one of the args for add_additional_attributes to the object the method is called on. Used self in method instead of previous arg.

--- lib/movie.rb
+++ lib/movie.rb
@@ -9,24 +9,27 @@ class Movie
 				self.send(("#{key}="), value)
 			end
 		end
-		add_additional_attributes(self, movie_hash)
+		self.add_additional_attributes(movie_hash)
 		@@all << self
 	end
 
 	def self.create_from_scraper(movie_array)
 		movie_array.each do |movie_hash|
-			
 			current_movies_in_all = self.all.collect do |movie|
 				movie.title
 			end
+			#binding.pry
 			if current_movies_in_all.include?(movie_hash[:title])
-				movie_in_all = self.all.search do |movie|
+				#binding.pry
+				movie_in_all = self.all.find do |movie|
 					movie.title == movie_hash[:title]
 				end
-				add_additional_attributes(movie_in_all, movie_hash)
+				movie_in_all.add_additional_attributes(movie_hash)
+
 			else
 				Movie.new(movie_hash)
 			end
+			#binding.pry
 			
 		end
 		#binding.pry
@@ -37,19 +40,21 @@ class Movie
 		@@all
 	end
 
-	def add_additional_attributes(movie, movie_hash)
+	def add_additional_attributes(movie_hash)
 		if movie_hash[:chain] == "cinemark"
-			movie.cinemark = movie_hash[:show_times]
+
+			self.cinemark = movie_hash[:show_times]
 		elsif movie_hash[:chain] == "regal"
-			movie.regal = movie_hash[:show_times]
+			self.regal = movie_hash[:show_times]
 		end
 	end
 
 	def self.show_times
 		self.all.each_with_index do |movie, i|
 			puts "#{i+1}. #{movie.title}"
-			puts "Carmike: #{movie.cinemark}"
+			puts "Cinemark: #{movie.cinemark}"
 			puts "Regal: #{movie.regal}"
 		end
 	end
+
 end
