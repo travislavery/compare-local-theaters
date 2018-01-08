f34738f433fdfc3bce46ead0ee54ed9894742b09
TravisA <github email address>
2018-01-07 19:24:09 -0700

    clarified responsibility for self.send to initialize method.

--- lib/movie.rb
+++ lib/movie.rb
@@ -2,14 +2,14 @@ class Movie
 	attr_accessor :title, :rating, :carmike, :megaplex
 
 	@@all = []
-	def initialize(title)
-		@title = title
+	def initialize(movie_hash)
+		movie_hash.each {|key, value| self.send(("#{key}="), value)}
 		@@all << self
 	end
 
-	def self.create_from_scraper(movie_object)
-		movie_object.each do |key, value|
-			self.send(("#{key}="), value)
+	def self.create_from_scraper(movie_array)
+		movie_array.each do |movie|
+			Movie.new(movie)
 		end
 	end
 
