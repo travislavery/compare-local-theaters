7451d5b5c2f9077844e5528f7013d07060847a73
TravisA <github email address>
2018-01-07 21:59:54 -0700

    calling on additional methods

--- lib/command-line-interface.rb
+++ lib/command-line-interface.rb
@@ -1,3 +1,4 @@
+require "pry"
 require_relative "./movie.rb"
 require_relative "./theater.rb"
 require_relative "./regal_scraper.rb"
@@ -10,6 +11,14 @@ cinemark_movies = Cinemark_scraper.scrape_movies("https://www.cinemark.com/utah/
 regal_theater = Regal_scraper.scrape_theater("https://www.regmovies.com/theaters/regal-crossroad-14-rpx/c00662716661")
 regal_movies = Regal_scraper.scrape_movies("https://www.regmovies.com/theaters/regal-crossroad-14-rpx/c00662716661")
 
-Movie.create_from_scraper(cinemark_movies)
+cinemark = Theater.create_from_scraper(cinemark_theater)
+regal = Theater.create_from_scraper(regal_theater)
+
 Movie.create_from_scraper(regal_movies)
+Movie.create_from_scraper(cinemark_movies)
+
+binding.pry
 Movie.show_times
+
+#cinemark.movies
+#cinemark.movie_titles
