fe7a1e0a1d53581e36ea505f7bead085d832e800
TravisA <github email address>
2018-01-10 19:11:44 -0700

    created environment file

---
+++ lib/environment.rb
@@ -0,0 +1,15 @@
+require "colorize"
+require "nokogiri"
+require "pry"
+require "open-uri"
+
+
+require_relative "./movie.rb"
+require_relative "./theater.rb"
+require_relative "./scrapers/regal_scraper.rb"
+require_relative "./scrapers/cinemark_scraper.rb"
+require_relative "./command-line-interface.rb"
+
+
+
+
